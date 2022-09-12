231fc0d8435fd1fb6b8c2f0c90f12f118518e6e2
Yuqian Jiang <597174820@qq.com>
2022-09-12 12:57:36 +0800

    Update dis_fit.r
    plot script add --type

--- scripts/dis_fit.r
+++ scripts/dis_fit.r
@@ -14,7 +14,9 @@ option_list = list(
     make_option(c("-s","--split"), action = "store_true", default = FALSE,
     help = "split by name [default]", metavar = "character"),
     make_option(c("-b","--binwidth"), type = "numeric", default = NULL,
-    help = "output histogram bin width", metavar = "numeric")
+    help = "output histogram bin width", metavar = "numeric"),
+    make_option(c("-t","--type"), type = "character", default = NULL,
+    help = "output according to different type", metavar = "character")
 );
 
 opt_parser = OptionParser(option_list=option_list);
@@ -24,17 +26,31 @@ if (is.null(opt$file)){
     print_help(opt_parser)
     stop ("At least one argument must be supplied (input file) .n", call.=FALSE)
 }
-wild <- read_tsv(opt$file, col_names = T, show_col_types = FALSE)
-wildfit <- ddply(wild, "type", summarise, grp.median = median(fit))
+all <- read_tsv(opt$file, col_names = T, show_col_types = FALSE)
 
-if (is.null(opt$binwidth)){
-    pfit <- ggplot(wild, aes(x = fit, fill = type)) +
-            geom_histogram(alpha = 0.5, position = "identity") +
-            geom_vline(data = wildfit, aes(xintercept = grp.median, color = type), linetype = "dashed")
-} else {
-    pfit <- ggplot(wild, aes(x = fit, fill = type)) +
-            geom_histogram(binwidth = opt$binwidth, alpha = 0.5, position = "identity") +
-            geom_vline(data = wildfit, aes(xintercept = grp.median, color = type), linetype = "dashed")
+if (opt$type == "type"){
+    wild <- subset(all, exist == "wild")
+    wildfit <- ddply(wild, "type", summarise, grp.median = median(fit))
+    if (is.null(opt$binwidth)){
+        pfit <- ggplot(wild, aes(x = fit, fill = type)) +
+                geom_histogram(alpha = 0.5, position = "identity") +
+                geom_vline(data = wildfit, aes(xintercept = grp.median, color = type), linetype = "dashed")
+    } else {
+        pfit <- ggplot(wild, aes(x = fit, fill = type)) +
+                geom_histogram(binwidth = opt$binwidth, alpha = 0.5, position = "identity") +
+                geom_vline(data = wildfit, aes(xintercept = grp.median, color = type), linetype = "dashed")
+    }
+} else if (opt$type == "exist") {
+    existfit <- ddply(all, "exist", summarise, grp.median = median(fit))
+    if (is.null(opt$binwidth)){
+        pfit <- ggplot(all, aes(x = fit, fill = exist)) +
+                geom_histogram(alpha = 0.5, position = "identity") +
+                geom_vline(data = existfit, aes(xintercept = grp.median, color = exist), linetype = "dashed")
+    } else {
+        pfit <- ggplot(all, aes(x = fit, fill = exist)) +
+                geom_histogram(binwidth = opt$binwidth, alpha = 0.5, position = "identity") +
+                geom_vline(data = existfit, aes(xintercept = grp.median, color = exist), linetype = "dashed")
+    }
 }
 
 if (opt$split) {
